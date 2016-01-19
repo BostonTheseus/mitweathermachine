44b6baecfee53b3fc859783e2accb96690605d24
Robert Morris <obviouslyrob@gmail.com>
2016-01-19 21:37:34 +0000

    Set up the controller and rooted it to index adding placeholder content on the view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Mitweathermachine::Application.routes.draw do
+  root 'machines#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
