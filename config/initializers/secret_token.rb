0e1ea0b6d510986ecffd58b624bfd91605a657e7
Robert Morris <obviouslyrob@gmail.com>
2016-01-19 20:48:46 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mitweathermachine::Application.config.secret_key_base = 'abb8d297d355a6c23320af64f0869e6e022c6faee2ecbaba6bef4cf9c8c52fea989c405494f5cbb05f35ddc7e1e7b3f6b62aa84bf7b1184fbf29bd1dd0d65930'
